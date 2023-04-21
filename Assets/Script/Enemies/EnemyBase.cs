561ea60b04ced54ad78db9813dc178ee95b1b99a
DevZilio <105543270+DevZilio@users.noreply.github.com>
2023-04-21 23:11:31 +0100

    Implementing Gun Shoot, Gamage Gun and Enemy Health
    Using KeyCode S, create a gameObject that go throw the Vector1 and hits the enemy dealing damage to its destruction.
    
    Red Flash still bug, after two shoots, keep red collor.

--- Assets/Script/Enemies/EnemyBase.cs
+++ Assets/Script/Enemies/EnemyBase.cs
@@ -10,6 +10,8 @@ public class EnemyBase : MonoBehaviour
     public Animator animator;
     public string triggerAttack = "Attack";
 
+    public HealthBase healthBase;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log(collision.transform.name);
@@ -28,5 +30,10 @@ public class EnemyBase : MonoBehaviour
     private void PlayAttackAnimation()
     {
         animator.SetTrigger(triggerAttack);
+    }
+
+    public void Damage(int amount)
+    {
+healthBase.Damage(amount);
     }
 }
