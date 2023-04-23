c0e83a27e335dce17d7b61be4061f51e9aa0d6f3
DevZilio <105543270+DevZilio@users.noreply.github.com>
2023-04-23 21:18:24 +0100

    Adding item manager to count numbers of coins on interface

--- Assets/Script/Items/ItemManager.cs
+++ Assets/Script/Items/ItemManager.cs
@@ -1,27 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using DevZilio.Core.Singleton;
+using TMPro;
+using UnityEngine;
 
 public class ItemManager : Singleton<ItemManager>
 {
 
-public int coins;
+    public int coins;
+
+    public TextMeshProUGUI uiTextCoins;
 
 
-private void Start()
-{
-    Reset();
-}
+    private void Start()
+    {
+        Reset();
+    }
 
-private void Reset()
-{
-    coins = 0;
-}
+    private void Reset()
+    {
+        coins = 0;
+        UpdateUI();
+    }
 
-public void AddCoins(int amount = 1)
-{
-    coins += amount;
-}
+    public void AddCoins(int amount = 1)
+    {
+        coins += amount;
+        UpdateUI();
+    }
 
+
+    public void UpdateUI()
+    {
+        uiTextCoins.text = coins.ToString();
+
+    }
 }
