7e54707973e9ed53b27a829620cb6bd50acef944
DevZilio <105543270+DevZilio@users.noreply.github.com>
2023-04-25 23:28:36 +0100

    Bug Correction
    Using ChatGPT, the bug when the character receives sequencial hits in short time, the function Flash() was saving double animation and losting the standard color, then keeping in red color.

--- Assets/Script/Utils/FlashColor.cs
+++ Assets/Script/Utils/FlashColor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using DG.Tweening;
 using UnityEngine;
 
@@ -8,34 +9,44 @@ public class FlashColor : MonoBehaviour
 {
     public List<SpriteRenderer> spriteRenderers;
     public Color color = Color.red;
-    public float duration = .2f;
+    public float duration = .02f;
 
-    private Tween _currentTween;
+    private Tween[] _currentTweens;
 
     private void OnValidate()
     {
         spriteRenderers = new List<SpriteRenderer>();
+
         foreach (var child in transform.GetComponentsInChildren<SpriteRenderer>())
         {
             spriteRenderers.Add(child);
         }
 
-
+        _currentTweens = new Tween[spriteRenderers.Count];
     }
 
 
     public void Flash()
     {
         //codigo para evitar o DOTween executar antes de ele mesmo terminar
-        if (_currentTween != null)
+        if (_currentTweens != null)
         {
-            _currentTween.Kill();
+            foreach (var t in _currentTweens)
+            {
+                if (t != null)
+                {
+                    t.Kill();
+                }
+            }
             spriteRenderers.ForEach(i => i.color = Color.white);
+
         }
-        
+
         foreach (var s in spriteRenderers)
         {
-           _currentTween = s.DOColor(color, duration).SetLoops(2, LoopType.Yoyo);
+            var tween = s.DOColor(color, duration).SetLoops(2, LoopType.Yoyo);
+            tween.SetId(s.GetInstanceID());
+            _currentTweens[spriteRenderers.IndexOf(s)] = tween;
         }
     }
 }
