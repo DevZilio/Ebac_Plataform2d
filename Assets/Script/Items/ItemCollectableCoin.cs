8c366f22ad28fe09cd0e5b2fab8f23f9d2c58722
DevZilio <105543270+DevZilio@users.noreply.github.com>
2023-05-06 22:06:59 +0100

    Update ItemCollectableCoin.cs
    add Coments

--- Assets/Script/Items/ItemCollectableCoin.cs
+++ Assets/Script/Items/ItemCollectableCoin.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class ItemCollectableCoin : ItemCollectableBase
 {
-
+    // Public variable to store the animator component
     public Animator animator;
 
+    // Override of the OnCollect function from the ItemCollectableBase class
     protected override void OnCollect()
     {
+        // Call the base implementation of OnCollect to handle common behavior
         base.OnCollect();
-        animator.SetTrigger("Collected");
+
+        // Trigger the "Collected" animation state in the animator component
+        //Temporariamente desativada para coanimator.SetTrigger("Collected");
+
+        // Destroy the game object after 1 second
         Destroy(gameObject, 1);
 
+        // Call the AddCoins function in the ItemManager singleton
         ItemManager.Instance.AddCoins();
     }
 
+    // Update function called every frame
     private void Update()
     {
-        // Verifica se a animação terminou e reativa o Collider2D
+        // Check if the "Collected" animation state has finished and re-enable the Collider2D component
         if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1 && !animator.IsInTransition(0))
         {
             Collider2D collider = GetComponent<Collider2D>();
