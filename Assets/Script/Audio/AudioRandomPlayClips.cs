10df1f1e2ed1aa5ce8eebc933f16e5e338809ec6
DevZilio <105543270+DevZilio@users.noreply.github.com>
2023-05-07 19:18:06 +0100

    Character Walk Sound Added

---
+++ Assets/Script/Audio/AudioRandomPlayClips.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioRandomPlayClips : MonoBehaviour
+{
+    public List<AudioClip> audioClipList;
+
+    public AudioSource audioSource;
+
+    public void PlayRandom()
+    {
+        audioSource.clip = audioClipList[Random.Range(0, audioClipList.Count)];
+        audioSource.Play();
+    }
+}
